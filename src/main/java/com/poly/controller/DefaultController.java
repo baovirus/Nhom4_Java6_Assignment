879169ef09b0999bf64d87e4a10248c5f2a850db
baovirus <thaibaoo2105@gmail.com>
2025-03-29 23:44:27 +0700

    hiển thị sản phẩm, danh mục trang index

--- src/main/java/com/poly/controller/DefaultController.java
+++ src/main/java/com/poly/controller/DefaultController.java
@@ -1,12 +1,43 @@
 package com.poly.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.poly.entity.Category;
+import com.poly.entity.Product;
+import com.poly.service.CategoryService;
+import com.poly.service.ProductService;
+
+
 
 @Controller
 public class DefaultController {
+	@Autowired
+	private CategoryService categoryService;
+	@Autowired
+	private ProductService productService;
+	
 	@RequestMapping("/")
-	public String Test() {
+	public String index(Model model) {
+		
+		
+		List<Category> categories = categoryService.findAll();
+		List<Product> products = productService.findAll();
+		model.addAttribute("categories", categories);
+		model.addAttribute("products", products);
+		return ("index");
+	}
+	@RequestMapping("/template")
+	public String template() {
 		return ("template");
+	}
+	@RequestMapping("/login")
+	public String login() {
+		return ("auth/login");
 	}
 }
