80207df8cf428ad228e788ea3ed8ffa939c00353
baovirus <thaibaoo2105@gmail.com>
2025-04-03 00:42:32 +0700

    refactor code
    chỉnh sửa lay out single page

--- src/main/java/com/poly/controller/DefaultController.java
+++ src/main/java/com/poly/controller/DefaultController.java
@@ -5,14 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.poly.entity.Account;
 import com.poly.entity.Category;
 import com.poly.entity.Product;
 import com.poly.service.AccountService;
@@ -38,78 +32,60 @@ public class DefaultController {
 		return ("product/list");
 	}
 
-	@RequestMapping("/category/{id}")
-	public String FindProductByCategory(@PathVariable String id, Model model) {
+	/*
+	 * @RequestMapping("/category/{id}") public String
+	 * FindProductByCategory(@PathVariable String id, Model model) {
+	 * 
+	 * List<Category> categories = categoryService.findAll(); List<Product> products
+	 * = productService.getProductsByCategory(id); model.addAttribute("categories",
+	 * categories); model.addAttribute("products", products); return
+	 * ("product/list"); }
+	 */
 
-		List<Category> categories = categoryService.findAll();
-		List<Product> products = productService.getProductsByCategory(id);
-		model.addAttribute("categories", categories);
-		model.addAttribute("products", products);
-		return ("product/list");
-	}
-
-	@RequestMapping("/product/{id}")
-	public String ProductDetail(@PathVariable Integer id, Model model) {
-
-		List<Category> categories = categoryService.findAll();
-		Product product = productService.findById(id);
-		// Lấy danh sách sản phẩm cùng danh mục
-		List<Product> relatedProducts = productService.getProductsByCategory(product.getCategory().getId());
-		model.addAttribute("categories", categories);
-		model.addAttribute("product", product);
-		model.addAttribute("relatedProducts", relatedProducts);
-		return ("product/detail");
-	}
+	/*
+	 * @RequestMapping("/product/{id}") public String ProductDetail(@PathVariable
+	 * Integer id, Model model) {
+	 * 
+	 * List<Category> categories = categoryService.findAll(); Product product =
+	 * productService.findById(id); // Lấy danh sách sản phẩm cùng danh mục
+	 * List<Product> relatedProducts =
+	 * productService.getProductsByCategory(product.getCategory().getId());
+	 * model.addAttribute("categories", categories); model.addAttribute("product",
+	 * product); model.addAttribute("relatedProducts", relatedProducts); return
+	 * ("product/detail"); }
+	 */
 
 	@RequestMapping("/template")
 	public String template() {
 		return ("template");
 	}
 
-	@RequestMapping("/login")
-	public String login() {
-		return ("account/login");
-	}
-
-	@GetMapping("/register")
-	public String registerForm(Model model) {
-		model.addAttribute("user", new Account());
-		return "account/register";
-	}
-
-	@PostMapping("/register")
-	public String processRegister(@ModelAttribute("user") Account user, BindingResult result, Model model) {
-		if (result.hasErrors()) {
-			return "account/register";
-		}
-
-		try {
-			accountService.save(user);
-		} catch (RuntimeException e) {
-			model.addAttribute("error", e.getMessage());
-			return "account/register";
-		}
-
-		return "redirect:/register?success";
-	}
-
-	@RequestMapping("/forgot-password")
-	public String forgot_password() {
-		return ("account/forgot-password");
-	}
-
-	@RequestMapping("/profile")
-	public String profile() {
-		return ("account/profile");
-	}
-
-	@RequestMapping("/my-cart")
-	public String my_cart() {
-		return ("cart/my-cart");
-	}
-
-	@RequestMapping("/my-order")
-	public String my_order() {
-		return ("order/my-order");
-	}
+	/*
+	 * /* @RequestMapping("/login") public String login() { return
+	 * ("account/login"); }
+	 * 
+	 * @GetMapping("/register") public String registerForm(Model model) {
+	 * model.addAttribute("user", new Account()); return "account/register"; }
+	 * 
+	 * @PostMapping("/register") public String
+	 * processRegister(@ModelAttribute("user") Account user, BindingResult result,
+	 * Model model) { if (result.hasErrors()) { return "account/register"; }
+	 * 
+	 * try { accountService.save(user); } catch (RuntimeException e) {
+	 * model.addAttribute("error", e.getMessage()); return "account/register"; }
+	 * 
+	 * return "redirect:/register?success"; }
+	 * 
+	 * @RequestMapping("/forgot-password") public String forgot_password() { return
+	 * ("account/forgot-password"); }
+	 * 
+	 * @RequestMapping("/profile") public String profile() { return
+	 * ("account/profile"); }
+	 * 
+	 * @RequestMapping("/my-cart") public String my_cart() { return
+	 * ("cart/my-cart"); }
+	 * 
+	 * @RequestMapping("/my-order") public String my_order() { return
+	 * ("order/my-order"); }
+	 */
 }
